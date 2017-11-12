e0b51950261028f76c111e10c99dbed65d5c90f7
Robert Edison <robert@robertedison.co.uk>
2017-11-12 10:58:09 +0000

    Add timer code
    Added timer to MainWindow. Got the ComboBox to trigger the timer and
    update the TextBlock.

--- RestYourEyes/RestYourEyes/MainWindow.xaml.cs
+++ RestYourEyes/RestYourEyes/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,6 +22,7 @@ namespace RestYourEyes
     public partial class MainWindow : Window
     {
         public List<TimeOption> TimeOptions { get; set; }
+        public Timer eyeRestTimer = new Timer();
 
         public MainWindow()
         {
@@ -30,11 +32,6 @@ namespace RestYourEyes
             TimeOptions.Add(new TimeOption("30 minutes", 1800000));
             InitializeComponent();
             this.DataContext = this;
-        }
-
-        private void Set_Timer(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("You clicked set timer");
         }
 
         private void License(object sender, RoutedEventArgs e)
@@ -48,7 +45,27 @@ namespace RestYourEyes
 
         private void TimeChoice_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            MessageBox.Show(TimeChoice.SelectedValue.ToString());
+            TimeOption selectedTimeOption;
+            selectedTimeOption = (TimeOption) TimeChoice.SelectedItem;
+            setTimer(selectedTimeOption.Duration);
+            TimerStatus.Text = selectedTimeOption.Name;
+        }
+
+        private void setTimer(int timerDuration)
+        {
+            if (eyeRestTimer.Enabled == false)
+            {
+
+                eyeRestTimer.Elapsed += OnTimedEvent;
+                eyeRestTimer.Interval = timerDuration;
+                eyeRestTimer.Enabled = true;
+            }
+        }
+
+        private void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            MessageBox.Show("Please take a break and rest your eyes.", "Rest Your Eyes - Message");
+            eyeRestTimer.Enabled = false;
         }
 
     }
